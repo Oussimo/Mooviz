f27ad9681bfb6271df584b998a204d5fa653057a
Oussimo <94698826+Oussimo@users.noreply.github.com>
2024-09-09 22:03:12 +0000

    fix options

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -18,14 +18,14 @@ const MovieDetails = () => {
 	}
 
 	return (
-		<div className="p-4 bg-background text-textPrimary max-w-screen-md mx-auto">
-			<div className="flex flex-col lg:flex-row items-center lg:items-start">
+		<div className="p-2 bg-background text-textPrimary max-w-screen-md mx-auto">
+			<div className="flex flex-col md:gap-2 md:flex-row items-center md:items-start">
 				<img
-					className="w-full lg:w-1/3 rounded-lg"
+					className="w-full md:w-1/3 rounded-lg"
 					src={movie.Poster}
 					alt={`${movie.Title} Poster`}
 				/>
-				<div className="lg:ml-8 mt-4 lg:mt-0 w-full">
+				<div className="md:ml-8 mt-4 md:mt-0 w-full">
 					<h1 className="text-3xl font-heading mb-2">{`${movie.Title} (${movie.Year})`}</h1>
 					<div className="flex items-center mb-4">
 						<span className="bg-secondary text-background text-lg font-bold rounded-full px-3 py-1 mr-2">
@@ -34,12 +34,13 @@ const MovieDetails = () => {
 						<span className="text-textSecondary">{movie.imdbVotes} votes</span>
 					</div>
 					<p className="mb-4 text-lg">{movie.Plot}</p>
-					<div className="grid lg:grid-cols-2 items-start gap-2 text-textPrimary mb-4 ">
+					<div className="grid md:grid-cols-2 items-start gap-2 text-textPrimary mb-4 ">
 						<div className="flex items-start gap-2 ">
 							<FaRegClock className="text-lg" /> Runtime: {movie.Runtime}
 						</div>
 						<div className="flex items-start gap-2 ">
-							<FaRegCalendarAlt className="text-lg" /> Released: {movie.Released}
+							<FaRegCalendarAlt className="text-lg" /> Released:{" "}
+							{movie.Released}
 						</div>
 						<div className="flex items-start gap-2 ">
 							<FaRegStar className="text-lg" /> Genre: {movie.Genre}
@@ -53,7 +54,6 @@ const MovieDetails = () => {
 						<div className="flex items-start gap-2 ">
 							<FaFilm className="text-lg" /> Writer: {movie.Writer}
 						</div>
-						
 					</div>
 					<h2 className="text-xl font-heading mb-2">Cast</h2>
 					<p>{movie.Actors}</p>
